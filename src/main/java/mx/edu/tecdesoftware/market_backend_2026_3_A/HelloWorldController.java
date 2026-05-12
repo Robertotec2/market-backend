a358874a80787ce269bbf7aab1de57bed2ef8ad8
Robertotec2 <robertobalmessol1s@gmail.com>
2026-05-12 08:28:50 -0600

    test: adding HelloWorldController to test Spring

---
+++ src/main/java/mx/edu/tecdesoftware/market_backend_2026_3_A/HelloWorldController.java
@@ -0,0 +1,13 @@
+package mx.edu.tecdesoftware.market_backend_2026_3_A;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hola")
+    public String saludar() {
+        return "Hello World";
+    }
+}
