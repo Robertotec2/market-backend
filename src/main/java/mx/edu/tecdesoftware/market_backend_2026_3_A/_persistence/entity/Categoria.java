d52f5eb09da689c29b254edca757dd8ddc921e96
Robertotec2 <robertobalmessol1s@gmail.com>
2026-05-25 08:55:26 -0600

    feat(persistence): add base entities
    - Create core domain entities (Cliente, Categoria).
    - Add intermediate entity CompraProducto.
    - Implement composite key CompraProductoPK (@Embeddable and Serializable).

---
+++ src/main/java/mx/edu/tecdesoftware/market_backend_2026_3_A/_persistence/entity/Categoria.java
@@ -0,0 +1,41 @@
+package mx.edu.tecdesoftware.market_backend_2026_3_A._persistence.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table
+public class Categoria {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column (name ="id_categoria")
+    private int idCategoria;
+
+
+    private String descripcion;
+
+    private Boolean estado;
+
+    public int getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(int idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
+}
