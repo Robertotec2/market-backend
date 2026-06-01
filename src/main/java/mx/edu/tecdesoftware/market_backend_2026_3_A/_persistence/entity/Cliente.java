f33dd9e00604510df1e217a499b19c6c24b5c12f
Robertotec2 <robertobalmessol1s@gmail.com>
2026-06-01 08:40:17 -0600

    Subiendo mis cambios

---
+++ src/main/java/mx/edu/tecdesoftware/market_backend_2026_3_A/_persistence/entity/Cliente.java
@@ -0,0 +1,66 @@
+package mx.edu.tecdesoftware.market_backend_2026_3_A._persistence.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table (name = "clientes")
+public class Cliente {
+    @Id
+    private Integer id;
+
+    private String nombre;
+    private String apellido;
+    private Long celular;
+    private String direccion;
+
+    @Column(name = "correo_electronico")
+    private String correoelectronico;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public Long getCelular() {
+        return celular;
+    }
+
+    public void setCelular(Long celular) {
+        this.celular = celular;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getCorreoelectronico() {
+        return correoelectronico;
+    }
+
+    public void setCorreoelectronico(String correoelectronico) {
+        this.correoelectronico = correoelectronico;
+    }
+}
---
+++ src/main/java/mx/edu/tecdesoftware/market_backend_2026_3_A/_persistence/entity/Compra.java
@@ -0,0 +1,49 @@
+
+package mx.edu.tecdesoftware.market_backend_2026_3_A._persistence.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "compras")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Compra {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_compra")
+    private Integer idCompra;
+
+    @Column(name = "id_cliente")
+    private String idCliente;
+
+    @Column(name = "fecha")
+    private LocalDate fecha;
+
+    @Column(name = "medio_pago")
+    private String medioPago;
+
+    @Column(name = "comentario")
+    private String comentario;
+
+    @Column(name = "estado")
+    private String estado;
+
+    //Me falto el comit
+
+    // Relacion con cliente, Muchas compras para un cliente
+    @ManyToOne
+    @JoinColumn( name = "id_cliente", insertable = false, updatable = false)
+    private Cliente cliente ;
+
+    //una compra tiene muchos productos
+    @OneToMany(mappedBy = "compra ")
+    private List<CompraProducto> productos;
+
+}
